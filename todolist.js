cafe369bf422806b5beafebd6f2184d78637d815
comelf <leebyungwoo@gmail.com>
2014-07-17 17:20:32 +0900

    1주차

--- todolist.js
+++ todolist.js
@@ -1,3 +1,22 @@
-function aa(){
-	
+var KEYCODE = {
+	ENTER : 13
+}
+
+document.addEventListener("DOMContentLoaded",function(){
+	document.getElementById("new-todo").addEventListener("keydown",function(e){
+		if(e.keyCode === KEYCODE.ENTER){
+			var text = document.getElementById("new-todo").value;
+			var todo = makeTODO(text);
+			document.getElementById("todo-list").insertAdjacentHTML("afterbegin",todo);
+			document.getElementById("new-todo").value = "";
+		}
+	})
+})
+
+function makeTODO(todo){
+	var source   = document.getElementById("entry-template").innerHTML;
+	var template = Handlebars.compile(source);
+	var context  = {title: todo}
+	var html     = template(context);
+	return html;
 }
