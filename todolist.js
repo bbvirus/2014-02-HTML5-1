367f850e9a24f5fe927074aba348d4d07c28f831
Jehyeok <hook3748@gmail.com>
2014-07-24 13:53:19 +0900

    메서드 분리

--- todolist.js
+++ todolist.js
@@ -1,39 +1,41 @@
+// 상수
 var KEY_CODE_ENTER = 13;
 
-// <li class="completed">
-// 						<div class="view">
-// 							<input class="toggle" type="checkbox" checked>
-// 							<label>타이틀</label>
-// 							<button class="destroy"></button>
-// 						</div>
-// 					</li>
 var todoList = document.getElementById('todo-list');
 var todoInput = document.getElementById('new-todo');
 
-todoInput.addEventListener('keydown', function(e) {
+function getTodo(inputValue) {
+	var li = document.createElement('li');
+	var div = document.createElement('div');
+	div.className = 'view';
+
+	var input = document.createElement('input');
+	input.className = 'toggle';
+	input.type = 'checkbox';
+
+	var label = document.createElement('label');
+	label.innerHTML = inputValue;
+
+	var button = document.createElement('button');
+	button.className = 'destroy';
+
+	div.appendChild(input);
+	div.appendChild(label);
+	div.appendChild(button);
+	li.appendChild(div);
+
+	return li;
+}
+
+function insertTODO(e) {
 	var keyCode = e.keyCode;
 	
 	if (keyCode === KEY_CODE_ENTER) {
-		var li = document.createElement('li');
-		var div = document.createElement('div');
-		div.className = 'view';
-
-		var input = document.createElement('input');
-		input.className = 'toggle';
-		input.type = 'checkbox';
-
-		var label = document.createElement('label');
-		label.innerHTML = this.value;
-
-		var button = document.createElement('button');
-		button.className = 'destroy';
-
-		div.appendChild(input);
-		div.appendChild(label);
-		div.appendChild(button);
-		li.appendChild(div);
+		var li = getTodo(this.value);
+		
 		todoList.appendChild(li);
-
-		todoInput.value = '';
+		this.value = '';
 	}
-}, false);
+}
+
+todoInput.addEventListener('keydown', insertTODO, false);
